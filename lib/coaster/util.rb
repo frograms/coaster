173744ef4b885a998da27d0a5ffc02bb29db6e73
buzz <buzz@watcha.com>
2022-08-09 15:38:04 +0900

    flatten hashify

---
+++ lib/coaster/util.rb
@@ -0,0 +1,22 @@
+module Coaster
+  module Util
+    FLATTEN_HASH_DELIMITER = '.'.freeze
+
+    class << self
+      def flatten_hashify(object, delimiter: FLATTEN_HASH_DELIMITER, breadcrumbs: [])
+        case object
+        when Hash
+          object.each_with_object({}) do |(key, value), memo|
+            memo.merge!(flatten_hashify(value, breadcrumbs: breadcrumbs + [key]))
+          end
+        when Array
+          object.each.with_index(1).with_object({}) do |(element, ix), memo|
+            memo.merge!(flatten_hashify(element, breadcrumbs: breadcrumbs + [ix]))
+          end
+        else
+          {breadcrumbs.join(delimiter) => object}
+        end
+      end
+    end
+  end
+end
