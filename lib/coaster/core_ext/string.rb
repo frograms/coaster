5a86a82097e77bde6e9f87e43ea1fe344e027585
potter <potter@watcha.com>
2023-09-19 11:04:24 +0900

    :wrench: Split by char type

--- lib/coaster/core_ext/string.rb
+++ lib/coaster/core_ext/string.rb
@@ -63,8 +63,12 @@ class String
     end
   end
 
-  # @note 반각 문자 -> 전각 문자(숫자, 알파벳, 기호 only)
-  def to_full_characters(with_symbol: false)
+  # @note 반각 문자 -> 전각 문자
+  # @param [TrueClass, FalseClass] alpha
+  # @param [TrueClass, FalseClass] number
+  # @param [TrueClass, FalseClass] symbol
+  # @return [String]
+  def to_full_characters(alpha: true, number: true, symbol: false)
     result = String.new
     return result if self.blank?
 
@@ -73,10 +77,10 @@ class String
       full_ord = half_ord + 0xfee0
       char_ord = case half_ord
                  when 0x20 then 0x3000
-                 when ('0'.ord)..('9'.ord) then full_ord
-                 when ('A'.ord)..('Z'.ord) then full_ord
-                 when ('a'.ord)..('z'.ord) then full_ord
-                 when ('!'.ord)..('~'.ord) then with_symbol ? full_ord : half_ord
+                 when ('0'.ord)..('9'.ord) then number ? full_ord : half_ord
+                 when ('A'.ord)..('Z'.ord) then alpha ? full_ord : half_ord
+                 when ('a'.ord)..('z'.ord) then alpha ? full_ord : half_ord
+                 when ('!'.ord)..('~'.ord) then symbol ? full_ord : half_ord
                  else half_ord
                  end
       result << char_ord.chr('UTF-8')
@@ -84,8 +88,12 @@ class String
     result
   end
 
-  # @note 전각 문자 -> 반각 문자 (숫자, 알파벳 only)
-  def to_half_characters(with_symbol: false)
+  # @note 전각 문자 -> 반각 문자
+  # @param [TrueClass, FalseClass] alpha
+  # @param [TrueClass, FalseClass] number
+  # @param [TrueClass, FalseClass] symbol
+  # @return [String]
+  def to_half_characters(alpha: true, number: true, symbol: false)
     result = String.new
     return result if self.blank?
 
@@ -94,10 +102,10 @@ class String
       half_ord = full_ord - 0xfee0
       char_ord = case full_ord
                  when 0x3000 then 0x20
-                 when ('0'.ord + 0xfee0)..('9'.ord + 0xfee0) then half_ord
-                 when ('A'.ord + 0xfee0)..('Z'.ord + 0xfee0) then half_ord
-                 when ('a'.ord + 0xfee0)..('z'.ord + 0xfee0) then half_ord
-                 when ('!'.ord + 0xfee0)..('~'.ord + 0xfee0) then with_symbol ? half_ord : full_ord
+                 when ('0'.ord + 0xfee0)..('9'.ord + 0xfee0) then number ? half_ord : full_ord
+                 when ('A'.ord + 0xfee0)..('Z'.ord + 0xfee0) then alpha ? half_ord : full_ord
+                 when ('a'.ord + 0xfee0)..('z'.ord + 0xfee0) then alpha ? half_ord : full_ord
+                 when ('!'.ord + 0xfee0)..('~'.ord + 0xfee0) then symbol ? half_ord : full_ord
                  else full_ord
                  end
       result << char_ord.chr('UTF-8')
