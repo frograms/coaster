8e8c362c1e973f649b2734414ce60c29ec996449
potter <potter@watcha.com>
2023-09-19 11:05:09 +0900

    :test_tube: Fix spec

--- test/test_string.rb
+++ test/test_string.rb
@@ -63,17 +63,16 @@ module Coaster
       full_seq = '　！＂＃＄％＆＇（）＊＋，－．／０１２３４５６７８９：；＜＝＞？＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ［＼］＾＿｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ｛｜｝～'
       not_target_half_seq = ((33..255).map { |c| c.chr('UTF-8') }.join.chars - half_seq.chars).join + '일이삼いちにさんイチニサン一二三'
       mixed_str = half_seq + not_target_half_seq
-      with_symbol = true
       # half -> full 1:1 잘 변환되는가?
-      assert_equal half_seq.to_full_characters(with_symbol:), full_seq
+      assert_equal half_seq.to_full_characters(symbol: true), full_seq
       # half -> full -> half 변환 시, 원래 string 유지되는가?
-      assert_equal half_seq.to_full_characters(with_symbol:).to_half_characters(with_symbol:), half_seq
+      assert_equal half_seq.to_full_characters(symbol: true).to_half_characters(symbol: true), half_seq
       # not_target string은 to full 변환 시도시 원본이 유지되는가?
-      assert_equal not_target_half_seq.to_full_characters(with_symbol:), not_target_half_seq
+      assert_equal not_target_half_seq.to_full_characters(symbol: true), not_target_half_seq
       # target / not_target이 섞여있는 문장에서, target'만' full로 변환되는가?
-      assert_equal mixed_str.to_full_characters(with_symbol:), full_seq + not_target_half_seq
+      assert_equal mixed_str.to_full_characters(symbol: true), full_seq + not_target_half_seq
       # target / not_target이 섞여있는 문장에서, half -> full -> half 변환시, 원래 string 유지되는가?
-      assert_equal mixed_str.to_full_characters(with_symbol:).to_half_characters(with_symbol:), mixed_str
+      assert_equal mixed_str.to_full_characters(symbol: true).to_half_characters(symbol: true), mixed_str
       # 공백문자열에 다른게 추가되지는 않는가?
       assert_equal ''.to_full_characters, ''
 
@@ -100,25 +99,24 @@ module Coaster
       full_seq = '　！＂＃＄％＆＇（）＊＋，－．／０１２３４５６７８９：；＜＝＞？＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ［＼］＾＿｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ｛｜｝～'
       not_target_full_seq = ((33..255).map { |c| (c + 0xfee0).chr('UTF-8') }.join.chars - full_seq.chars).join + '일이삼いちにさんイチニサン一二三'
       mixed_str = full_seq + not_target_full_seq
-      with_symbol = true
       # full -> half 1:1 잘 변환되는가?
-      expect(full_seq.to_half_characters(with_symbol:)).to eq half_seq
-      assert_equal full_seq.to_half_characters(with_symbol:), half_seq
+      expect(full_seq.to_half_characters(symbol: true)).to eq half_seq
+      assert_equal full_seq.to_half_characters(symbol: true), half_seq
       # full -> half -> full 변환 시, 원래 string 유지되는가?
-      expect(full_seq.to_half_characters(with_symbol:).to_full_characters(with_symbol:)).to eq full_seq
-      assert_equal full_seq.to_half_characters(with_symbol:).to_full_characters(with_symbol:), full_seq
+      expect(full_seq.to_half_characters(symbol: true).to_full_characters(symbol: true)).to eq full_seq
+      assert_equal full_seq.to_half_characters(symbol: true).to_full_characters(symbol: true), full_seq
       # not_target string은 to half 변환 시도시 원본이 유지되는가?
-      expect(not_target_full_seq.to_half_characters(with_symbol:)).to eq not_target_full_seq
-      assert_equal not_target_full_seq.to_half_characters(with_symbol:), not_target_full_seq
+      expect(not_target_full_seq.to_half_characters(symbol: true)).to eq not_target_full_seq
+      assert_equal not_target_full_seq.to_half_characters(symbol: true), not_target_full_seq
       # target / not_target이 섞여있는 문장에서, target'만' half로 변환되는가?
-      expect(mixed_str.to_half_characters(with_symbol:)).to eq half_seq + not_target_full_seq
-      assert_equal mixed_str.to_half_characters(with_symbol:), half_seq + not_target_full_seq
+      expect(mixed_str.to_half_characters(symbol: true)).to eq half_seq + not_target_full_seq
+      assert_equal mixed_str.to_half_characters(symbol: true), half_seq + not_target_full_seq
       # target / not_target이 섞여있는 문장에서, full -> half -> full 변환시, 원래 string 유지되는가?
-      expect(mixed_str.to_half_characters(with_symbol:).to_full_characters(with_symbol:)).to eq mixed_str
-      assert_equal mixed_str.to_half_characters(with_symbol:).to_full_characters(with_symbol:), mixed_str
+      expect(mixed_str.to_half_characters(symbol: true).to_full_characters(symbol: true)).to eq mixed_str
+      assert_equal mixed_str.to_half_characters(symbol: true).to_full_characters(symbol: true), mixed_str
       # 공백문자열에 다른게 추가되지는 않는가?
-      expect(''.to_half_characters(with_symbol:)).to eq ''
-      assert_equal ''.to_half_characters(with_symbol:), ''
+      expect(''.to_half_characters(symbol: true)).to eq ''
+      assert_equal ''.to_half_characters(symbol: true), ''
 
       # strips tags correctly
       # 알라딘 "독거소녀 삐삐" 중 contents
